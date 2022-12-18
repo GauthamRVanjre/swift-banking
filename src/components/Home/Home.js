9e1439a0c0b0de213662751a670034d8a02b585b
Gautham Vanjre <vanjregautham@gmail.com>
2022-12-18 20:00:16 +0530

    completed home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import illustration from "../../assets/image-mockups.png";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home-layout">
+        <div className="home-content">
+          <h1>Welcome to swift banking</h1>
+          <p>A banking system for swift and safe payments</p>
+          <div>
+            <Link to="/Transactions">
+              <button className="transaction-btn">View Transactions</button>
+            </Link>
+            <Link to="/Payment">
+              <button className="payment-btn">Make Payments</button>
+            </Link>
+          </div>
+        </div>
+        <div className="home-illustration">
+          <img src={illustration} alt="illustration" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
